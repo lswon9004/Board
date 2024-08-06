ebe1d446f89fd9613eb2de86fde12db3acb439c1
swon <lswon9004@gmail.com>
2024-08-06 18:17:03 +0900

    줄수정

--- src/main/java/member/board/controller/BoardController.java
+++ src/main/java/member/board/controller/BoardController.java
@@ -53,28 +53,22 @@ public class BoardController {
 		
 		//글이 있는지 체크
 		int count = service.count();
-		if(count > 0) {
-		
-		int perPage = 10; // 한 페이지에 보일 글의 갯수
-		int startRow = (page - 1) * perPage;//인덱스 번호
-		
-		List<BoardDto> boardList = service.boardList(startRow);
-		
-		m.addAttribute("bList", boardList);
-
-		int pageNum = 5;
-		int totalPages = count / perPage + (count % perPage > 0 ? 1 : 0); //전체 페이지 수
-		
-		int begin = (page - 1) / pageNum * pageNum + 1;
-		int end = begin + pageNum -1;
-		if(end > totalPages) {
-			end = totalPages;
-		}
-		 m.addAttribute("begin", begin);
-		 m.addAttribute("end", end);
-		 m.addAttribute("pageNum", pageNum);
-		 m.addAttribute("totalPages", totalPages);
-		
+		if(count > 0) {	
+			int perPage = 10; // 한 페이지에 보일 글의 갯수
+			int startRow = (page - 1) * perPage;//인덱스 번호		
+			List<BoardDto> boardList = service.boardList(startRow);		
+			m.addAttribute("bList", boardList);
+			int pageNum = 5;
+			int totalPages = count / perPage + (count % perPage > 0 ? 1 : 0); //전체 페이지 수	
+			int begin = (page - 1) / pageNum * pageNum + 1;
+			int end = begin + pageNum -1;
+			if(end > totalPages) {
+				end = totalPages;
+			}
+			m.addAttribute("begin", begin);
+			m.addAttribute("end", end);
+			m.addAttribute("pageNum", pageNum);
+			m.addAttribute("totalPages", totalPages);		
 		}
 		m.addAttribute("count", count);
 		return "board/list";
